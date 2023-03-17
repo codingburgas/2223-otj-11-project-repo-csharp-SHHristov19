f353005aec7313f5101abc5fbbdf67e369d2aa1d
SHHristov19 <SHHristov19@codingburgas.bg>
2023-03-17 17:48:52 +0200

    Add empty classes in Repositories in DAL

---
+++ Project/Univers.DAL/Repositories/SpecialityRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Univers.DAL.Repositories
+{
+    internal class SpecialityRepository
+    {
+    }
+}
