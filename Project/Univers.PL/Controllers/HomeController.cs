0023f301281a4d819f9932d1980e08b738f70655
SHHristov19 <SHHristov19@codingburgas.bg>
2023-04-12 18:20:04 +0300

    Add menu for the student views * Add calendar for student home page

---
+++ Project/Univers.PL/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Univers.PL.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult StaffHome()
+        {
+            return View();
+        }
+        
+        public ActionResult StudentHome()
+        {
+            return View();
+        }
+    }
+}
